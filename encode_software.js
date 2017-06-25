0c7d1257961679f05b37e68a776e8f7a9a91ebac
jyoshikuta <jyoshikuta1909@gmail.com>
2017-06-25 17:03:07 +0800

    Add files via upload

---
+++ encode_software.js
@@ -0,0 +1,95 @@
+var encrypt = null;
+var userinput = "";
+var result = "";
+ 
+function validateInput(inputField, displayField) {
+    var re = /[a-z]+/;
+    if (re.test(inputField.value)) {
+        displayField.innerHTML = inputField.value;
+    }
+    else {
+        displayField.innerHTML = "Wrong!!";
+    }
+}
+ 
+function display(choice) {
+    var message;
+	var inputField = document.getElementById('input'); 
+    var displayField = document.getElementById('display');
+    switch (choice) {
+        case "new":
+            encrypt = new Encrypt();
+            message = "code: " + encrypt.showCode();
+            break;
+                 
+        case "save":
+            if (encrypt == null) {
+                message = "No Encrypt object can save!!";
+            }
+            else {
+                document.cookie = "code=" + encrypt.showCode();
+                message = "The code is saved.";
+            }
+            break;
+ 
+        case "load":
+            var t = document.cookie;
+            if (t == "") {
+                message = "Load denied!!";
+            }
+            else {
+                encrypt = new Encrypt();
+                t = t.substring(5);
+                encrypt.setCode(t)
+                message = "code: " + encrypt.showCode();
+            }
+            break;
+                 
+        case "encode":
+            userinput = inputField.value;
+             
+            if (userinput == "") {
+                message = "No input string!!";
+            }
+            else {
+                if (encrypt == null) {
+                    message = "No encrypt object!!";
+                }
+                else {
+                    result = encrypt.toEncode(userinput);
+                    message = "encoded result: " + result;
+                }
+            }
+            break;
+     
+        case "decode":
+            userinput = inputField.value;
+             
+            if (userinput == "") {
+                message = "No input string!!";
+            }
+            else {
+                if (encrypt == null) {
+                    message = "No encrypt object!!";
+                }
+                else {
+                    result = encrypt.toDecode(userinput);
+                    message = "decoded result: " + result;
+                }
+            }
+            break;
+ 
+        case "clear":
+            inputField.value = "";
+            encrypt = null;
+            userinput = "";
+            result = "";
+            message = "It's done.";            
+            break;
+                 
+        default:
+            message = "Wrong choice!!";
+    }
+     
+    displayField.innerHTML = message;
+}
---
+++ encrypt.js
@@ -0,0 +1,60 @@
+function Encrypt() {
+    this.code = "abcdefghijklmnopqrstuvwxyz".split("", 26);
+    var i = this.code.length;
+    while (i) {
+        var j = parseInt(Math.random() * i);
+        var x = this.code[--i];
+        this.code[i] = this.code[j];
+        this.code[j] = x;
+    }
+     
+    this.alph = "abcdefghijklmnopqrstuvwxyz".split("", 26);
+}
+ 
+Encrypt.prototype.showCode = function() {
+    return this.code.join("");
+}
+ 
+Encrypt.prototype.setCode = function(c) {
+    this.code = c.split("", 26);
+}
+ 
+Encrypt.prototype.toEncode = function(str) {
+    var result = "";
+    var i = 0;
+    var len = str.length;
+    while (i < len) {
+        var regExp = /^[a-z]+$/;
+        if (regExp.test(str[i])) {
+            var j = this.alph.indexOf(str[i])
+            result += this.code[j];
+        }
+        else {
+            result += str[i];
+        }
+             
+        i++;
+    }
+     
+    return result;
+}
+ 
+Encrypt.prototype.toDecode = function(str) {
+    var result = "";
+    var i = 0;
+    var len = str.length;
+    while (i < len) {
+        var regExp = /^[a-z]+$/;
+        if (regExp.test(str[i])) {
+            var j = this.code.indexOf(str[i]);
+            result += this.alph[j];
+        }
+        else {
+            result += str[i];
+        }
+             
+        i++;
+    }
+     
+    return result;
+}
